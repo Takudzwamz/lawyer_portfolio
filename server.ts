f8ba4642848b6385bc14f0a65d71b7fa9cf1eafa
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-09-01 23:26:12 +0200

    make it better

--- server.ts
+++ server.ts
@@ -11,6 +11,7 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import sslRedirect from 'heroku-ssl-redirect';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -23,6 +24,7 @@ export function app(): express.Express {
     bootstrap: AppServerModule,
   }));
 
+  server.use(sslRedirect());
   server.set('view engine', 'html');
   server.set('views', distFolder);
 

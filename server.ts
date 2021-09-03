06d41f3719523cc7f0ed74165d448c072b6f83da
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-09-03 14:18:03 +0200

    revert

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const forceSsl = require('force-ssl-heroku');
+  // const forceSsl = require('force-ssl-heroku');
   const distFolder = join(process.cwd(), 'dist/spaceman/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
@@ -26,7 +26,7 @@ export function app(): express.Express {
   }));
 
   // server.use(sslRedirect());
-  server.use(forceSsl);
+  // server.use(forceSsl);
   // lets go
   server.set('view engine', 'html');
   server.set('views', distFolder);
